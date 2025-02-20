1bc6ea9597b700bfaa8fd8887784ed6110d85850
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2025-02-20 07:24:00 -0300

    test: create getWeatherByCityService test

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,16 @@
+import { api } from './api';
+import { getWeatherByCityService } from './getWeatherByCityService';
+import { mockWeatherAPIResponse } from '@__tests__/__mocks__/mockWeatherAPIResponse';
+
+describe('Service: getWeatherByCityService', () => {
+  it('should be return weather api data formatted', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const response = await getWeatherByCityService({
+      latitude: 123,
+      longitude: 456,
+    });
+
+    expect(response).toHaveProperty('today');
+  });
+});
